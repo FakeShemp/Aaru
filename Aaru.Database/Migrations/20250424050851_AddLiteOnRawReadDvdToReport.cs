c18ef0e823a5c8b24737a0e1efc2dbf9507d97f5
Natalia Portillo <claunia@claunia.com>
2025-04-24 06:20:41 +0100

    [Database] Added migrations for Lite-On RAW DVD readings device reports.

---
+++ Aaru.Database/Migrations/20250424050851_AddLiteOnRawReadDvdToReport.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aaru.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLiteOnRawReadDvdToReport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "LiteOnReadRawDVDData",
+                table: "TestedMedia",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SupportsLiteOnReadRawDVD",
+                table: "TestedMedia",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LiteOnReadRawDVDData",
+                table: "TestedMedia");
+
+            migrationBuilder.DropColumn(
+                name: "SupportsLiteOnReadRawDVD",
+                table: "TestedMedia");
+        }
+    }
+}
