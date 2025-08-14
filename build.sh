fb09614577627fdbd421de14e59bbfe6f94a9488
Natalia Portillo <claunia@claunia.com>
2025-08-14 03:35:26 +0100

    Update build script.

--- build.sh
+++ build.sh
@@ -8,49 +8,31 @@ mkdir -p build
 cd Aaru
 for conf in Debug Release;
 do
- for distro in alpine-arm64 alpine-arm alpine-x64 linux-arm64 linux-arm linux-x64 osx-arm64 osx-x64 win-arm64 win-arm win-x64 win-x86 debian-arm debian-arm64 debian-x64 rhel-arm64 rhel-x64 sles-x64;
+ for distro in linux-arm64 linux-arm linux-x64 osx-x64 osx-arm64 win-arm64 win-x64 win-x86;
  do
-  dotnet publish -f net7.0 -r ${distro} -c ${conf}
+  dotnet publish -f net8.0 -r ${distro} -c ${conf}
 
 # Package the Linux packages (stopped working)
-#  if [[ ${distro} == alpine* ]] || [[ ${distro} == linux* ]]; then
-#    pkg="tarball"
-#  elif [[ ${distro} == win* ]] || [[ ${distro} == osx* ]]; then
-#    pkg="zip"
+  if [[ ${distro} == alpine* ]] || [[ ${distro} == linux* ]]; then
+    dotnet tarball -f net8.0 -r ${distro} -c ${conf} -o ../build
+    dotnet rpm -f net8.0 -r ${distro} -c ${conf} -o ../build
+    dotnet deb -f net8.0 -r ${distro} -c ${conf} -o ../build
+  elif [[ ${distro} == win* ]] || [[ ${distro} == osx* ]]; then
+    dotnet zip -f net8.0 -r ${distro} -c ${conf} -o ../build
 #  elif [[ ${distro} == rhel* ]] || [[ ${distro} == sles* ]]; then
 #    pkg="rpm"
 #  else
 #    pkg="deb"
-#  fi
-#  dotnet ${pkg} -f net7.0 -r ${distro} -c ${conf} -o ../build
-#
-
-# Package the Linux packages using MSBuild
-  if [[ ${distro} == alpine* ]] || [[ ${distro} == linux* ]]; then
-    task="CreateTarball"
-    extension="tar.gz"
-  elif [[ ${distro} == win* ]] || [[ ${distro} == osx* ]]; then
-    task="CreateZip"
-    extension="zip"
-  elif [[ ${distro} == rhel* ]] || [[ ${distro} == sles* ]]; then
-    task="CreateRpm"
-    extension="rpm"
-  else
-    task="CreateDeb"
-    extension="deb"
   fi
 
-  dotnet msbuild Aaru.csproj /t:${task} /p:TargetFramework=net7.0 /p:RuntimeIdentifier=${distro} /p:Configuration=${conf}
-  mv bin/${conf}/net7.0/${distro}/*.${extension} ../build
-
- done
+  done
 done
 
 cd ..
 
 # If we are compiling on Linux check if we are on Arch Linux and then create the Arch Linux package as well
 if [[ ${OS_NAME} == Linux ]]; then
- OS_RELEASE=$(pcregrep -o1 -e "^ID=(?<distro_id>\w+)" /etc/os-release)
+ OS_RELEASE=`pcregrep -o1 -e "^ID=(?<distro_id>\w+)" /etc/os-release`
 
  if [[ ${OS_RELEASE} != arch ]]; then
   exit 0
@@ -87,10 +69,10 @@ mkdir -p build/macos/Aaru.app/Contents/Resources
 mkdir -p build/macos/Aaru.app/Contents/MacOS
 cp Aaru/Aaru.icns build/macos/Aaru.app/Contents/Resources
 cp Aaru/Info.plist build/macos/Aaru.app/Contents
-cp -r Aaru/bin/Release/net7.0/osx-x64/publish/* build/macos/Aaru.app/Contents/MacOS
+cp -r Aaru/bin/Release/net8.0/osx-x64/publish/* build/macos/Aaru.app/Contents/MacOS
 rm -Rf build/macos-dbg/Aaru.app
 mkdir -p build/macos-dbg/Aaru.app/Contents/Resources
 mkdir -p build/macos-dbg/Aaru.app/Contents/MacOS
 cp Aaru/Aaru.icns build/macos-dbg/Aaru.app/Contents/Resources
 cp Aaru/Info.plist build/macos-dbg/Aaru.app/Contents
-cp -r Aaru/bin/Debug/net7.0/osx-x64/publish/* build/macos-dbg/Aaru.app/Contents/MacOS
+cp -r Aaru/bin/Debug/net8.0/osx-x64/publish/* build/macos-dbg/Aaru.app/Contents/MacOS
