053bb611f24d190f227fda949cea17bc841171bb
Natalia Portillo <claunia@claunia.com>
2025-09-01 06:10:13 +0100

    [ARC] Implement GetEntryNumber.

--- Aaru.Archives/Arc/Files.cs
+++ Aaru.Archives/Arc/Files.cs
@@ -1,3 +1,4 @@
+using System;
 using Aaru.CommonTypes.Enums;
 
 namespace Aaru.Archives;
@@ -18,6 +19,31 @@ public sealed partial class Arc
         fileName = _entries[entryNumber].Filename;
 
         return ErrorNumber.NoError;
+    }
+
+    /// <inheritdoc />
+    public ErrorNumber GetEntryNumber(string fileName, bool caseInsensitiveMatch, out int entryNumber)
+    {
+        entryNumber = -1;
+
+        if(!Opened) return ErrorNumber.NotOpened;
+
+        if(entryNumber < 0 || entryNumber >= _entries.Count) return ErrorNumber.OutOfRange;
+
+        StringComparison comparison = caseInsensitiveMatch
+                                          ? StringComparison.CurrentCultureIgnoreCase
+                                          : StringComparison.CurrentCulture;
+
+        for(int i = 0, count = _entries.Count; i < count; i++)
+        {
+            if(!_entries[i].Filename.Equals(fileName, comparison)) continue;
+
+            entryNumber = i;
+
+            return ErrorNumber.NoError;
+        }
+
+        return ErrorNumber.NoSuchFile;
     }
 
 #endregion
