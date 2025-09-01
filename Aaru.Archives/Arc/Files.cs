9563794e9c6824383accd1d5f14a12afbb2fc6ab
Natalia Portillo <claunia@claunia.com>
2025-09-01 06:07:31 +0100

    [ARC] Implement GetFilename.

---
+++ Aaru.Archives/Arc/Files.cs
@@ -0,0 +1,24 @@
+using Aaru.CommonTypes.Enums;
+
+namespace Aaru.Archives;
+
+public sealed partial class Arc
+{
+#region IArchive Members
+
+    /// <inheritdoc />
+    public ErrorNumber GetFilename(int entryNumber, out string fileName)
+    {
+        fileName = null;
+
+        if(!Opened) return ErrorNumber.NotOpened;
+
+        if(entryNumber < 0 || entryNumber >= _entries.Count) return ErrorNumber.OutOfRange;
+
+        fileName = _entries[entryNumber].Filename;
+
+        return ErrorNumber.NoError;
+    }
+
+#endregion
+}
