28f6643870b72c4882f8ff13ee26248379a508eb
Natalia Portillo <claunia@claunia.com>
2025-09-01 03:21:28 +0100

    [ARC] Add ARC archive plugin skeleton.

---
+++ Aaru.Archives/Arc/Arc.cs
@@ -0,0 +1,27 @@
+using System;
+using Aaru.CommonTypes.Interfaces;
+
+namespace Aaru.Archives;
+
+public partial class Arc : IArchive
+{
+    const string MODULE_NAME = "arc Archive Plugin";
+
+#region IArchive Members
+
+    /// <inheritdoc />
+    public string Name => "arc";
+    /// <inheritdoc />
+    public Guid Id => new("D5C49A41-B10D-4DFE-B75E-3DAD11818818");
+    /// <inheritdoc />
+    public string Author => Authors.NataliaPortillo;
+    /// <inheritdoc />
+    public bool Opened { get; }
+    /// <inheritdoc />
+    public ArchiveSupportedFeature ArchiveFeatures =>
+        ArchiveSupportedFeature.SupportsCompression | ArchiveSupportedFeature.SupportsFilenames;
+    /// <inheritdoc />
+    public int NumberOfEntries { get; }
+
+#endregion
+}
