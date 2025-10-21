8ad7260f442e7d4e99e0b6de497dace274392999
Natalia Portillo <claunia@claunia.com>
2025-10-21 13:31:04 +0100

    Remove cuetools.net submodule

--- Aaru.Compression/cuetools.net/CUETools.Codecs.Flake/SeekPoint.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CUETools.Codecs.Flake
-{
-    public struct SeekPoint
-    {
-        public long number;
-        public long offset;
-        public int framesize;
-    }
-}
--- Aaru.Compression/cuetools.net/CUETools.Codecs/CUEToolsTagger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CUETools.Codecs
-{
-    public enum CUEToolsTagger
-    {
-        TagLibSharp = 0,
-        APEv2 = 1,
-        ID3v2 = 2,
-    }
-}
