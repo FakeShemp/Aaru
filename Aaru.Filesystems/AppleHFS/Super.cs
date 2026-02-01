5e2806973fd8ce2b3cc7fb199c3654bb7f15407e
Natalia Portillo <claunia@claunia.com>
2026-02-01 15:49:36 +0000

    [HFS] Implement StatFs.

---
+++ Aaru.Filesystems/AppleHFS/Super.cs
@@ -0,0 +1,64 @@
+// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : Super.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : Apple Hierarchical File System plugin.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2026 Natalia Portillo
+// ****************************************************************************/
+
+// ReSharper disable UnusedType.Local
+
+// ReSharper disable IdentifierTypo
+// ReSharper disable MemberCanBePrivate.Local
+
+using Aaru.CommonTypes.Enums;
+using Aaru.CommonTypes.Structs;
+
+namespace Aaru.Filesystems;
+
+// Information from Inside Macintosh
+// https://developer.apple.com/legacy/library/documentation/mac/pdf/Files/File_Manager.pdf
+public sealed partial class AppleHFS
+{
+    /// <inheritdoc />
+    public ErrorNumber StatFs(out FileSystemInfo stat)
+    {
+        stat = null;
+
+        if(!_mounted) return ErrorNumber.AccessDenied;
+
+        stat = new FileSystemInfo
+        {
+            Blocks         = _mdb.drNmAlBlks,
+            FilenameLength = 31,
+            Files          = _mdb.drFilCnt + _mdb.drDirCnt,
+            FreeBlocks     = _mdb.drFreeBks,
+            PluginId       = Id,
+            Type           = FS_TYPE
+        };
+
+        stat.FreeFiles = 0; // HFS doesn't track free files separately
+
+        return ErrorNumber.NoError;
+    }
+}
