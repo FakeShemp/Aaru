837cd3982e3831de093a37076bd2f71e62e4f95c
Natalia Portillo <claunia@claunia.com>
2026-02-04 14:55:42 +0000

    [bfs] Added missing on-disk structures.

--- Aaru.Filesystems/UNIXBFS/Consts.cs
+++ Aaru.Filesystems/UNIXBFS/Consts.cs
@@ -33,8 +33,32 @@ namespace Aaru.Filesystems;
 /// <summary>Implements detection of the UNIX boot filesystem</summary>
 public sealed partial class BFS
 {
-    const uint BFS_MAGIC    = 0x1BADFACE;
+    /// <summary>BFS magic number (little-endian)</summary>
+    const uint BFS_MAGIC = 0x1BADFACE;
+    /// <summary>BFS magic number (big-endian)</summary>
     const uint BFS_MAGIC_BE = 0xCEFAAD1B;
+
+    /// <summary>Block size in bits</summary>
+    const int BFS_BSIZE_BITS = 9;
+    /// <summary>Block size in bytes (512)</summary>
+    const int BFS_BSIZE = 1 << BFS_BSIZE_BITS;
+
+    /// <summary>Root inode number</summary>
+    const int BFS_ROOT_INO = 2;
+    /// <summary>Number of inodes per block (8)</summary>
+    const int BFS_INODES_PER_BLOCK = 8;
+
+    /// <summary>Directory vnode type</summary>
+    const uint BFS_VDIR = 2;
+    /// <summary>Regular file vnode type</summary>
+    const uint BFS_VREG = 1;
+
+    /// <summary>Maximum filename length</summary>
+    const int BFS_NAMELEN = 14;
+    /// <summary>Directory entry size in bytes</summary>
+    const int BFS_DIRENT_SIZE = 16;
+    /// <summary>Number of directory entries per block</summary>
+    const int BFS_DIRS_PER_BLOCK = 32;
 
     const string FS_TYPE = "bfs";
 }
--- Aaru.Filesystems/UNIXBFS/Structs.cs
+++ Aaru.Filesystems/UNIXBFS/Structs.cs
@@ -39,31 +39,93 @@ public sealed partial class BFS
 {
 #region Nested type: SuperBlock
 
+    /// <summary>BFS superblock layout on disk (512 bytes)</summary>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     [SwapEndian]
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     partial struct SuperBlock
     {
-        /// <summary>0x00, 0x1BADFACE</summary>
+        /// <summary>0x00, Magic number 0x1BADFACE</summary>
         public uint s_magic;
-        /// <summary>0x04, start in bytes of volume</summary>
+        /// <summary>0x04, Start offset in bytes of data area</summary>
         public uint s_start;
-        /// <summary>0x08, end in bytes of volume</summary>
+        /// <summary>0x08, End offset in bytes of data area</summary>
         public uint s_end;
-        /// <summary>0x0C, unknown :p</summary>
+        /// <summary>0x0C, Used for compaction (from inode)</summary>
         public uint s_from;
-        /// <summary>0x10, unknown :p</summary>
+        /// <summary>0x10, Used for compaction (to inode)</summary>
         public uint s_to;
-        /// <summary>0x14, unknown :p</summary>
+        /// <summary>0x14, Used for compaction (from block)</summary>
         public int s_bfrom;
-        /// <summary>0x18, unknown :p</summary>
+        /// <summary>0x18, Used for compaction (to block)</summary>
         public int s_bto;
-        /// <summary>0x1C, 6 bytes, filesystem name</summary>
+        /// <summary>0x1C, Filesystem name (6 bytes)</summary>
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
         public byte[] s_fsname;
-        /// <summary>0x22, 6 bytes, volume name</summary>
+        /// <summary>0x22, Volume name (6 bytes)</summary>
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
         public byte[] s_volume;
+        /// <summary>0x28, Padding to 512 bytes (118 uint32s = 472 bytes)</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 118)]
+        public uint[] s_padding;
+    }
+
+#endregion
+
+#region Nested type: Inode
+
+    /// <summary>BFS inode layout on disk (64 bytes)</summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [SwapEndian]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    partial struct Inode
+    {
+        /// <summary>0x00, Inode number</summary>
+        public ushort i_ino;
+        /// <summary>0x02, Unused</summary>
+        public ushort i_unused;
+        /// <summary>0x04, First data block</summary>
+        public uint i_sblock;
+        /// <summary>0x08, Last data block</summary>
+        public uint i_eblock;
+        /// <summary>0x0C, EOF offset in last block</summary>
+        public uint i_eoffset;
+        /// <summary>0x10, Vnode type (1=regular, 2=directory)</summary>
+        public uint i_vtype;
+        /// <summary>0x14, File mode/permissions</summary>
+        public uint i_mode;
+        /// <summary>0x18, Owner user ID</summary>
+        public uint i_uid;
+        /// <summary>0x1C, Owner group ID</summary>
+        public uint i_gid;
+        /// <summary>0x20, Number of links</summary>
+        public uint i_nlink;
+        /// <summary>0x24, Access time</summary>
+        public uint i_atime;
+        /// <summary>0x28, Modification time</summary>
+        public uint i_mtime;
+        /// <summary>0x2C, Change time</summary>
+        public uint i_ctime;
+        /// <summary>0x30, Padding (4 uint32s = 16 bytes)</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public uint[] i_padding;
+    }
+
+#endregion
+
+#region Nested type: DirectoryEntry
+
+    /// <summary>BFS directory entry (16 bytes)</summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [SwapEndian]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    partial struct DirectoryEntry
+    {
+        /// <summary>0x00, Inode number</summary>
+        public ushort ino;
+        /// <summary>0x02, Filename (14 bytes, null-padded)</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = BFS_NAMELEN)]
+        public byte[] name;
     }
 
 #endregion
