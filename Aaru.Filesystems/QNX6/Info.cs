b025b5cc8c34da53fcbc59730ce54bfc282e2d47
Natalia Portillo <claunia@claunia.com>
2026-02-04 13:11:58 +0000

    [qnx6] Added missing on-disk structures.

--- Aaru.Filesystems/QNX6/Info.cs
+++ Aaru.Filesystems/QNX6/Info.cs
@@ -59,11 +59,11 @@ public sealed partial class QNX6
 
         if(sector.Length < QNX6_SUPER_BLOCK_SIZE) return false;
 
-        AudiSuperBlock audiSb = Marshal.ByteArrayToStructureLittleEndian<AudiSuperBlock>(audiSector);
+        qnx6_mmi_super_block audiSb = Marshal.ByteArrayToStructureLittleEndian<qnx6_mmi_super_block>(audiSector);
 
-        SuperBlock qnxSb = Marshal.ByteArrayToStructureLittleEndian<SuperBlock>(sector);
+        qnx6_super_block qnxSb = Marshal.ByteArrayToStructureLittleEndian<qnx6_super_block>(sector);
 
-        return qnxSb.magic == QNX6_MAGIC || audiSb.magic == QNX6_MAGIC;
+        return qnxSb.sb_magic == QNX6_MAGIC || audiSb.sb_magic == QNX6_MAGIC;
     }
 
     /// <inheritdoc />
@@ -86,39 +86,39 @@ public sealed partial class QNX6
 
         if(sector.Length < QNX6_SUPER_BLOCK_SIZE) return;
 
-        AudiSuperBlock audiSb = Marshal.ByteArrayToStructureLittleEndian<AudiSuperBlock>(audiSector);
+        qnx6_mmi_super_block audiSb = Marshal.ByteArrayToStructureLittleEndian<qnx6_mmi_super_block>(audiSector);
 
-        SuperBlock qnxSb = Marshal.ByteArrayToStructureLittleEndian<SuperBlock>(sector);
+        qnx6_super_block qnxSb = Marshal.ByteArrayToStructureLittleEndian<qnx6_super_block>(sector);
 
-        bool audi = audiSb.magic == QNX6_MAGIC;
+        bool audi = audiSb.sb_magic == QNX6_MAGIC;
 
         if(audi)
         {
             sb.AppendLine(Localization.QNX6_Audi_filesystem);
-            sb.AppendFormat(Localization.Checksum_0_X8,       audiSb.checksum).AppendLine();
-            sb.AppendFormat(Localization.Serial_0_X16,        audiSb.checksum).AppendLine();
-            sb.AppendFormat(Localization._0_bytes_per_block,  audiSb.blockSize).AppendLine();
-            sb.AppendFormat(Localization._0_inodes_free_of_1, audiSb.freeInodes, audiSb.numInodes).AppendLine();
+            sb.AppendFormat(Localization.Checksum_0_X8,       audiSb.sb_checksum).AppendLine();
+            sb.AppendFormat(Localization.Serial_0_X16,        audiSb.sb_serial).AppendLine();
+            sb.AppendFormat(Localization._0_bytes_per_block,  audiSb.sb_blocksize).AppendLine();
+            sb.AppendFormat(Localization._0_inodes_free_of_1, audiSb.sb_free_inodes, audiSb.sb_num_inodes).AppendLine();
 
             sb.AppendFormat(Localization._0_blocks_1_bytes_free_of_2_3_bytes,
-                            audiSb.freeBlocks,
-                            audiSb.freeBlocks * audiSb.blockSize,
-                            audiSb.numBlocks,
-                            audiSb.numBlocks * audiSb.blockSize)
+                            audiSb.sb_free_blocks,
+                            audiSb.sb_free_blocks * audiSb.sb_blocksize,
+                            audiSb.sb_num_blocks,
+                            audiSb.sb_num_blocks * audiSb.sb_blocksize)
               .AppendLine();
 
             metadata = new FileSystem
             {
                 Type         = FS_TYPE,
-                Clusters     = audiSb.numBlocks,
-                ClusterSize  = audiSb.blockSize,
+                Clusters     = audiSb.sb_num_blocks,
+                ClusterSize  = audiSb.sb_blocksize,
                 Bootable     = true,
-                Files        = audiSb.numInodes - audiSb.freeInodes,
-                FreeClusters = audiSb.freeBlocks,
-                VolumeSerial = $"{audiSb.serial:X16}"
+                Files        = audiSb.sb_num_inodes - audiSb.sb_free_inodes,
+                FreeClusters = audiSb.sb_free_blocks,
+                VolumeSerial = $"{audiSb.sb_serial:X16}"
             };
 
-            //xmlFSType.VolumeName = CurrentEncoding.GetString(audiSb.id);
+            //xmlFSType.VolumeName = CurrentEncoding.GetString(audiSb.sb_id);
 
             information = sb.ToString();
 
@@ -126,39 +126,42 @@ public sealed partial class QNX6
         }
 
         sb.AppendLine(Localization.QNX6_filesystem);
-        sb.AppendFormat(Localization.Checksum_0_X8,     qnxSb.checksum).AppendLine();
-        sb.AppendFormat(Localization.Serial_0_X16,      qnxSb.checksum).AppendLine();
-        sb.AppendFormat(Localization.Created_on_0,      DateHandlers.UnixUnsignedToDateTime(qnxSb.ctime)).AppendLine();
-        sb.AppendFormat(Localization.Last_mounted_on_0, DateHandlers.UnixUnsignedToDateTime(qnxSb.atime)).AppendLine();
-        sb.AppendFormat(Localization.Flags_0_X8,        qnxSb.flags).AppendLine();
-        sb.AppendFormat(Localization.Version1_0_X4,     qnxSb.version1).AppendLine();
-        sb.AppendFormat(Localization.Version2_0_X4,     qnxSb.version2).AppendLine();
+        sb.AppendFormat(Localization.Checksum_0_X8, qnxSb.sb_checksum).AppendLine();
+        sb.AppendFormat(Localization.Serial_0_X16,  qnxSb.sb_serial).AppendLine();
+        sb.AppendFormat(Localization.Created_on_0,  DateHandlers.UnixUnsignedToDateTime(qnxSb.sb_ctime)).AppendLine();
 
-        //sb.AppendFormat("Volume ID: \"{0}\"", CurrentEncoding.GetString(qnxSb.volumeid)).AppendLine();
-        sb.AppendFormat(Localization._0_bytes_per_block,  qnxSb.blockSize).AppendLine();
-        sb.AppendFormat(Localization._0_inodes_free_of_1, qnxSb.freeInodes, qnxSb.numInodes).AppendLine();
+        sb.AppendFormat(Localization.Last_mounted_on_0, DateHandlers.UnixUnsignedToDateTime(qnxSb.sb_atime))
+          .AppendLine();
+
+        sb.AppendFormat(Localization.Flags_0_X8,    qnxSb.sb_flags).AppendLine();
+        sb.AppendFormat(Localization.Version1_0_X4, qnxSb.sb_version1).AppendLine();
+        sb.AppendFormat(Localization.Version2_0_X4, qnxSb.sb_version2).AppendLine();
+
+        //sb.AppendFormat("Volume ID: \"{0}\"", CurrentEncoding.GetString(qnxSb.sb_volumeid)).AppendLine();
+        sb.AppendFormat(Localization._0_bytes_per_block,  qnxSb.sb_blocksize).AppendLine();
+        sb.AppendFormat(Localization._0_inodes_free_of_1, qnxSb.sb_free_inodes, qnxSb.sb_num_inodes).AppendLine();
 
         sb.AppendFormat(Localization._0_blocks_1_bytes_free_of_2_3_bytes,
-                        qnxSb.freeBlocks,
-                        qnxSb.freeBlocks * qnxSb.blockSize,
-                        qnxSb.numBlocks,
-                        qnxSb.numBlocks * qnxSb.blockSize)
+                        qnxSb.sb_free_blocks,
+                        qnxSb.sb_free_blocks * qnxSb.sb_blocksize,
+                        qnxSb.sb_num_blocks,
+                        qnxSb.sb_num_blocks * qnxSb.sb_blocksize)
           .AppendLine();
 
         metadata = new FileSystem
         {
             Type             = FS_TYPE,
-            Clusters         = qnxSb.numBlocks,
-            ClusterSize      = qnxSb.blockSize,
+            Clusters         = qnxSb.sb_num_blocks,
+            ClusterSize      = qnxSb.sb_blocksize,
             Bootable         = true,
-            Files            = qnxSb.numInodes - qnxSb.freeInodes,
-            FreeClusters     = qnxSb.freeBlocks,
-            VolumeSerial     = $"{qnxSb.serial:X16}",
-            CreationDate     = DateHandlers.UnixUnsignedToDateTime(qnxSb.ctime),
-            ModificationDate = DateHandlers.UnixUnsignedToDateTime(qnxSb.atime)
+            Files            = qnxSb.sb_num_inodes - qnxSb.sb_free_inodes,
+            FreeClusters     = qnxSb.sb_free_blocks,
+            VolumeSerial     = $"{qnxSb.sb_serial:X16}",
+            CreationDate     = DateHandlers.UnixUnsignedToDateTime(qnxSb.sb_ctime),
+            ModificationDate = DateHandlers.UnixUnsignedToDateTime(qnxSb.sb_atime)
         };
 
-        //xmlFSType.VolumeName = CurrentEncoding.GetString(qnxSb.volumeid);
+        //xmlFSType.VolumeName = CurrentEncoding.GetString(qnxSb.sb_volumeid);
 
         information = sb.ToString();
     }
--- Aaru.Filesystems/QNX6/Structs.cs
+++ Aaru.Filesystems/QNX6/Structs.cs
@@ -34,73 +34,210 @@ namespace Aaru.Filesystems;
 /// <summary>Implements detection of QNX 6 filesystem</summary>
 public sealed partial class QNX6
 {
-#region Nested type: AudiSuperBlock
+#region Nested type: qnx6_root_node
 
+    /// <summary>QNX6 root node structure (used in superblock for inode, bitmap, longfile trees)</summary>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    readonly struct AudiSuperBlock
+    readonly struct qnx6_root_node
     {
-        public readonly uint  magic;
-        public readonly uint  checksum;
-        public readonly ulong serial;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-        public readonly byte[] spare1;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-        public readonly byte[] id;
-        public readonly uint     blockSize;
-        public readonly uint     numInodes;
-        public readonly uint     freeInodes;
-        public readonly uint     numBlocks;
-        public readonly uint     freeBlocks;
-        public readonly uint     spare2;
-        public readonly RootNode inode;
-        public readonly RootNode bitmap;
-        public readonly RootNode longfile;
-        public readonly RootNode unknown;
-    }
-
-#endregion
-
-#region Nested type: RootNode
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    readonly struct RootNode
-    {
+        /// <summary>Size of the tree</summary>
         public readonly ulong size;
+        /// <summary>Block pointers (16 direct pointers)</summary>
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        public readonly uint[] pointers;
+        public readonly uint[] ptr;
+        /// <summary>Number of indirect levels</summary>
         public readonly byte levels;
+        /// <summary>Mode</summary>
         public readonly byte mode;
+        /// <summary>Spare bytes</summary>
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
         public readonly byte[] spare;
     }
 
 #endregion
 
-#region Nested type: SuperBlock
+#region Nested type: qnx6_super_block
 
+    /// <summary>QNX6 superblock structure</summary>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    readonly struct SuperBlock
+    readonly struct qnx6_super_block
     {
-        public readonly uint   magic;
-        public readonly uint   checksum;
-        public readonly ulong  serial;
-        public readonly uint   ctime;
-        public readonly uint   atime;
-        public readonly uint   flags;
-        public readonly ushort version1;
-        public readonly ushort version2;
+        /// <summary>Magic number (0x68191122)</summary>
+        public readonly uint sb_magic;
+        /// <summary>Superblock checksum</summary>
+        public readonly uint sb_checksum;
+        /// <summary>Volume serial number</summary>
+        public readonly ulong sb_serial;
+        /// <summary>Creation time</summary>
+        public readonly uint sb_ctime;
+        /// <summary>Last access time</summary>
+        public readonly uint sb_atime;
+        /// <summary>Flags</summary>
+        public readonly uint sb_flags;
+        /// <summary>Filesystem version 1</summary>
+        public readonly ushort sb_version1;
+        /// <summary>Filesystem version 2</summary>
+        public readonly ushort sb_version2;
+        /// <summary>Volume ID (16 bytes)</summary>
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        public readonly byte[] volumeid;
-        public readonly uint     blockSize;
-        public readonly uint     numInodes;
-        public readonly uint     freeInodes;
-        public readonly uint     numBlocks;
-        public readonly uint     freeBlocks;
-        public readonly uint     allocationGroup;
-        public readonly RootNode inode;
-        public readonly RootNode bitmap;
-        public readonly RootNode longfile;
-        public readonly RootNode unknown;
+        public readonly byte[] sb_volumeid;
+        /// <summary>Block size in bytes</summary>
+        public readonly uint sb_blocksize;
+        /// <summary>Total number of inodes</summary>
+        public readonly uint sb_num_inodes;
+        /// <summary>Number of free inodes</summary>
+        public readonly uint sb_free_inodes;
+        /// <summary>Total number of blocks</summary>
+        public readonly uint sb_num_blocks;
+        /// <summary>Number of free blocks</summary>
+        public readonly uint sb_free_blocks;
+        /// <summary>Allocation group</summary>
+        public readonly uint sb_allocgroup;
+        /// <summary>Inode tree root</summary>
+        public readonly qnx6_root_node Inode;
+        /// <summary>Bitmap tree root</summary>
+        public readonly qnx6_root_node Bitmap;
+        /// <summary>Long filename tree root</summary>
+        public readonly qnx6_root_node Longfile;
+        /// <summary>Unknown tree root</summary>
+        public readonly qnx6_root_node Unknown;
+    }
+
+#endregion
+
+#region Nested type: qnx6_mmi_super_block
+
+    /// <summary>Audi MMI 3G superblock structure (different layout from plain QNX6)</summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    readonly struct qnx6_mmi_super_block
+    {
+        /// <summary>Magic number</summary>
+        public readonly uint sb_magic;
+        /// <summary>Superblock checksum</summary>
+        public readonly uint sb_checksum;
+        /// <summary>Volume serial number</summary>
+        public readonly ulong sb_serial;
+        /// <summary>Spare bytes</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+        public readonly byte[] sb_spare0;
+        /// <summary>ID (12 bytes)</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+        public readonly byte[] sb_id;
+        /// <summary>Block size in bytes</summary>
+        public readonly uint sb_blocksize;
+        /// <summary>Total number of inodes</summary>
+        public readonly uint sb_num_inodes;
+        /// <summary>Number of free inodes</summary>
+        public readonly uint sb_free_inodes;
+        /// <summary>Total number of blocks</summary>
+        public readonly uint sb_num_blocks;
+        /// <summary>Number of free blocks</summary>
+        public readonly uint sb_free_blocks;
+        /// <summary>Spare bytes</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public readonly byte[] sb_spare1;
+        /// <summary>Inode tree root</summary>
+        public readonly qnx6_root_node Inode;
+        /// <summary>Bitmap tree root</summary>
+        public readonly qnx6_root_node Bitmap;
+        /// <summary>Long filename tree root</summary>
+        public readonly qnx6_root_node Longfile;
+        /// <summary>Unknown tree root</summary>
+        public readonly qnx6_root_node Unknown;
+    }
+
+#endregion
+
+#region Nested type: qnx6_inode_entry
+
+    /// <summary>QNX6 inode entry (128 bytes each)</summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    readonly struct qnx6_inode_entry
+    {
+        /// <summary>File size</summary>
+        public readonly ulong di_size;
+        /// <summary>User ID</summary>
+        public readonly uint di_uid;
+        /// <summary>Group ID</summary>
+        public readonly uint di_gid;
+        /// <summary>File time (creation)</summary>
+        public readonly uint di_ftime;
+        /// <summary>Modification time</summary>
+        public readonly uint di_mtime;
+        /// <summary>Access time</summary>
+        public readonly uint di_atime;
+        /// <summary>Change time</summary>
+        public readonly uint di_ctime;
+        /// <summary>File mode (permissions)</summary>
+        public readonly ushort di_mode;
+        /// <summary>Extended mode</summary>
+        public readonly ushort di_ext_mode;
+        /// <summary>Block pointers (16 direct pointers)</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public readonly uint[] di_block_ptr;
+        /// <summary>Number of indirect levels</summary>
+        public readonly byte di_filelevels;
+        /// <summary>File status (0x01=directory, 0x02=deleted, 0x03=normal)</summary>
+        public readonly byte di_status;
+        /// <summary>Unknown bytes</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public readonly byte[] di_unknown2;
+        /// <summary>Zero padding</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+        public readonly uint[] di_zero2;
+    }
+
+#endregion
+
+#region Nested type: qnx6_dir_entry
+
+    /// <summary>QNX6 directory entry (32 bytes max, short filename)</summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    readonly struct qnx6_dir_entry
+    {
+        /// <summary>Inode number</summary>
+        public readonly uint de_inode;
+        /// <summary>Filename length</summary>
+        public readonly byte de_size;
+        /// <summary>Filename (max 27 characters)</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 27)]
+        public readonly byte[] de_fname;
+    }
+
+#endregion
+
+#region Nested type: qnx6_long_dir_entry
+
+    /// <summary>QNX6 long directory entry (for filenames longer than 27 chars)</summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    readonly struct qnx6_long_dir_entry
+    {
+        /// <summary>Inode number</summary>
+        public readonly uint de_inode;
+        /// <summary>Size indicator (0xFF for long entries)</summary>
+        public readonly byte de_size;
+        /// <summary>Unknown bytes</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public readonly byte[] de_unknown;
+        /// <summary>Long filename inode number (in longfile tree)</summary>
+        public readonly uint de_long_inode;
+        /// <summary>Checksum</summary>
+        public readonly uint de_checksum;
+    }
+
+#endregion
+
+#region Nested type: qnx6_long_filename
+
+    /// <summary>QNX6 long filename structure (stored in longfile tree)</summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    readonly struct qnx6_long_filename
+    {
+        /// <summary>Filename length</summary>
+        public readonly ushort lf_size;
+        /// <summary>Filename (max 510 characters)</summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 510)]
+        public readonly byte[] lf_fname;
     }
 
 #endregion
