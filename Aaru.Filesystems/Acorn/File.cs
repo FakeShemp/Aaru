4ef67f7c60b04cf6548debdff94033e4d3ea9846
Natalia Portillo <claunia@claunia.com>
2026-02-08 05:54:43 +0000

    Implement stat.

--- Aaru.Filesystems/Acorn/File.cs
+++ Aaru.Filesystems/Acorn/File.cs
@@ -33,12 +33,29 @@
 using System;
 using System.Collections.Generic;
 using Aaru.CommonTypes.Enums;
+using Aaru.CommonTypes.Structs;
 
 namespace Aaru.Filesystems;
 
 /// <inheritdoc />
 public sealed partial class AcornADFS
 {
+    /// <inheritdoc />
+    public ErrorNumber Stat(string path, out FileEntryInfo stat)
+    {
+        stat = null;
+
+        if(!_mounted) return ErrorNumber.AccessDenied;
+
+        ErrorNumber err = GetEntry(path, out DirectoryEntryInfo entry);
+
+        if(err != ErrorNumber.NoError) return err;
+
+        stat = EntryToFileEntryInfo(entry);
+
+        return ErrorNumber.NoError;
+    }
+
     /// <summary>Gets a directory entry by path</summary>
     /// <param name="path">Path to the entry</param>
     /// <param name="entry">Output directory entry</param>
@@ -109,5 +126,61 @@ public sealed partial class AcornADFS
         }
 
         return ErrorNumber.NoSuchFile;
+    }
+
+    /// <summary>Converts a directory entry to a FileEntryInfo structure</summary>
+    /// <param name="entry">Directory entry info</param>
+    /// <returns>FileEntryInfo structure</returns>
+    FileEntryInfo EntryToFileEntryInfo(DirectoryEntryInfo entry)
+    {
+        var info = new FileEntryInfo
+        {
+            Inode     = entry.IndAddr,
+            Length    = entry.Length,
+            BlockSize = _blockSize,
+            Blocks    = (entry.Length + _blockSize - 1) / _blockSize,
+            Links     = 1
+        };
+
+        // Set attributes
+        var attrs = (FileAttributes)entry.Attributes;
+
+        info.Attributes = attrs.HasFlag(FileAttributes.Directory)
+                              ? CommonTypes.Structs.FileAttributes.Directory
+                              : CommonTypes.Structs.FileAttributes.File;
+
+        if(attrs.HasFlag(FileAttributes.Locked)) info.Attributes |= CommonTypes.Structs.FileAttributes.ReadOnly;
+
+        // Check for symbolic link (filetype 0xFC0 = LinkFS)
+        if(HasFiletype(entry.LoadAddr) && GetFiletype(entry.LoadAddr) == FILETYPE_LINKFS)
+            info.Attributes |= CommonTypes.Structs.FileAttributes.Symlink;
+
+        // Convert RISC OS timestamp to DateTime if the file is stamped
+        // RISC OS timestamp: 40-bit centi-second value since 1 Jan 1900
+        // When load address bits [31:20] == 0xFFF, it's a stamped file:
+        // - load address bits [7:0] = top 8 bits of timestamp
+        // - exec address = bottom 32 bits of timestamp
+        if(!HasFiletype(entry.LoadAddr)) return info;
+
+        // File is stamped - extract timestamp
+        ulong timestamp = (ulong)(entry.LoadAddr & 0xFF) << 32 | entry.ExecAddr;
+
+        // Convert from centi-seconds to ticks (100ns intervals)
+        // 1 centi-second = 10,000,000 nanoseconds = 100,000 ticks
+        const long ticksPerCentiSecond = 100000;
+
+        // RISC OS epoch: 1 Jan 1900 00:00:00
+        var riscOsEpoch = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        var ticks = (long)(timestamp * ticksPerCentiSecond);
+
+        // Check if the resulting date would be valid
+        if(ticks >= 0 && ticks <= DateTime.MaxValue.Ticks - riscOsEpoch.Ticks)
+        {
+            info.LastWriteTimeUtc = riscOsEpoch.AddTicks(ticks);
+            info.AccessTimeUtc    = info.LastWriteTimeUtc;
+        }
+
+        return info;
     }
 }
--- Aaru.Filesystems/Acorn/Unimplemented.cs
+++ Aaru.Filesystems/Acorn/Unimplemented.cs
@@ -39,8 +39,6 @@ public sealed partial class AcornADFS
     /// <inheritdoc />
     public ErrorNumber StatFs(out FileSystemInfo stat) => throw new NotImplementedException();
 
-    /// <inheritdoc />
-    public ErrorNumber Stat(string path, out FileEntryInfo stat) => throw new NotImplementedException();
 
     /// <inheritdoc />
     public ErrorNumber ReadLink(string path, out string dest) => throw new NotImplementedException();
