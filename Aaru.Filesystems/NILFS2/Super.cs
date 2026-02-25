7b9464ebd908d22fd8cbd9b19dabdec8abfbdaf7
Natalia Portillo <claunia@claunia.com>
2026-02-25 20:46:46 +0000

    [NILFS2] Implement statfs.

--- Aaru.Filesystems/NILFS2/Super.cs
+++ Aaru.Filesystems/NILFS2/Super.cs
@@ -2,7 +2,7 @@
 // Aaru Data Preservation Suite
 // ----------------------------------------------------------------------------
 //
-// Filename       : Unimplemented.cs
+// Filename       : Super.cs
 // Author(s)      : Natalia Portillo <claunia@claunia.com>
 //
 // Component      : NILFS2 filesystem plugin.
@@ -26,15 +26,38 @@
 // Copyright © 2011-2026 Natalia Portillo
 // ****************************************************************************/
 
-// ReSharper disable UnusedMember.Local
-
-using System;
 using Aaru.CommonTypes.Enums;
+using Aaru.CommonTypes.Structs;
 
 namespace Aaru.Filesystems;
 
+/// <inheritdoc />
+/// <summary>Implements the New Implementation of a Log-structured File System v2</summary>
 public sealed partial class NILFS2
 {
     /// <inheritdoc />
-    public ErrorNumber ReadLink(string path, out string dest) => throw new NotImplementedException();
+    public ErrorNumber StatFs(out FileSystemInfo stat)
+    {
+        stat = null;
+
+        if(!_mounted) return ErrorNumber.AccessDenied;
+
+        stat = new FileSystemInfo
+        {
+            Blocks         = _superblock.dev_size / _blockSize,
+            FreeBlocks     = _superblock.free_blocks_count,
+            Files          = 0, // NILFS2 does not track total file count in the superblock
+            FreeFiles      = 0, // NILFS2 does not track free inode count in the superblock
+            FilenameLength = NILFS2_NAME_LEN,
+            Type           = FS_TYPE,
+            PluginId       = Id,
+            Id =
+            {
+                IsGuid = true,
+                uuid   = _superblock.uuid
+            }
+        };
+
+        return ErrorNumber.NoError;
+    }
 }
