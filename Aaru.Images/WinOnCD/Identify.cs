2eaaaf9f8c469594d7f16c5bbc49b91a79766815
Natalia Portillo <claunia@claunia.com>
2026-03-03 15:33:25 +0000

    [WinOnCD] Implement identify.

---
+++ Aaru.Images/WinOnCD/Identify.cs
@@ -0,0 +1,61 @@
+// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : Identify.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : Disc image plugins.
+//
+// --[ Description ] ----------------------------------------------------------
+//
+//     Identifies WinOnCD disc images.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2026 Natalia Portillo
+// ****************************************************************************/
+
+using System.IO;
+using System.Text;
+using Aaru.CommonTypes.Interfaces;
+using Aaru.Helpers;
+
+namespace Aaru.Images;
+
+public sealed partial class WinOnCD
+{
+#region IOpticalMediaImage Members
+
+    /// <inheritdoc />
+    public bool Identify(IFilter imageFilter)
+    {
+        Stream stream = imageFilter.GetDataForkStream();
+        stream.Seek(0, SeekOrigin.Begin);
+
+        if(stream.Length < SIGNATURE_LENGTH) return false;
+
+        var signatureBytes = new byte[SIGNATURE_LENGTH];
+        stream.EnsureRead(signatureBytes, 0, SIGNATURE_LENGTH);
+
+        string signature = Encoding.ASCII.GetString(signatureBytes);
+
+        return signature == SIGNATURE_ADAPTEC || signature.StartsWith(SIGNATURE_ROXIO);
+    }
+
+#endregion
+}
