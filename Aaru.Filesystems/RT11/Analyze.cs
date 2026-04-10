d87a64eda3a940b1aabdfde9844d55d881806596
Natalia Portillo <claunia@claunia.com>
2026-04-10 23:06:32 +0100

    [RT11] Add support for analyze command

---
+++ Aaru.Filesystems/RT11/Analyze.cs
@@ -0,0 +1,172 @@
+// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : Analyze.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : RT-11 filesystem plugin.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2026 Natalia Portillo
+// ****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aaru.CommonTypes;
+using Aaru.CommonTypes.Enums;
+using Aaru.CommonTypes.Structs;
+
+namespace Aaru.Filesystems;
+
+// Information from http://www.trailing-edge.com/~shoppa/rt11fs/
+/// <inheritdoc />
+public sealed partial class RT11
+{
+#region IReadOnlyFilesystem Members
+
+    /// <inheritdoc />
+    public ErrorNumber GetFilesWithAffectedSectors(IEnumerable<(ulong Start, ulong End)> sectorExtents,
+                                                   out List<FileSectorInfo>              files,
+                                                   InitProgressHandler                   initProgress   = null,
+                                                   UpdateProgressHandler                 updateProgress = null,
+                                                   PulseProgressHandler                  pulseProgress  = null,
+                                                   EndProgressHandler                    endProgress    = null)
+    {
+        files = [];
+
+        if(!_mounted) return ErrorNumber.AccessDenied;
+        if(sectorExtents is null) return ErrorNumber.InvalidArgument;
+
+        List<(ulong Start, ulong End)> normalizedExtents = NormalizeAnalyzeExtents(sectorExtents);
+
+        if(normalizedExtents.Count == 0 || _rootDirectoryCache is null || _rootDirectoryCache.Count == 0)
+            return ErrorNumber.NoError;
+
+        try
+        {
+            initProgress?.Invoke();
+            pulseProgress?.Invoke("/");
+
+            string[] orderedEntries = _rootDirectoryCache.Keys.OrderBy(static entry => entry,
+                                                                       StringComparer.CurrentCultureIgnoreCase)
+                                                         .ToArray();
+
+            long maximum = orderedEntries.Length > 0 ? orderedEntries.Length : 1;
+
+            for(var i = 0; i < orderedEntries.Length; i++)
+            {
+                string entryPath = "/" + orderedEntries[i];
+
+                updateProgress?.Invoke(entryPath, i + 1, maximum);
+
+                ErrorNumber errno = GetFileStartBlockAndLength(orderedEntries[i],
+                                                               out uint startBlock,
+                                                               out uint lengthInBlocks);
+
+                if(errno == ErrorNumber.NoSuchFile) continue;
+
+                if(errno != ErrorNumber.NoError) return errno;
+
+                AddOverlappingFile(entryPath, startBlock, lengthInBlocks, normalizedExtents, files);
+            }
+
+            return ErrorNumber.NoError;
+        }
+        finally
+        {
+            endProgress?.Invoke();
+        }
+    }
+
+#endregion
+
+    void AddOverlappingFile(string                                  path,          uint startBlock, uint lengthInBlocks,
+                            IReadOnlyList<(ulong Start, ulong End)> sectorExtents, List<FileSectorInfo> files)
+    {
+        if(lengthInBlocks == 0) return;
+
+        List<(ulong Start, ulong End)> overlaps = FindOverlappingExtents(startBlock, lengthInBlocks, sectorExtents);
+
+        if(overlaps.Count == 0) return;
+
+        files.Add(new FileSectorInfo
+        {
+            Path            = path,
+            Inode           = startBlock,
+            AffectedSectors = overlaps
+        });
+    }
+
+    List<(ulong Start, ulong End)> FindOverlappingExtents(uint startBlock, uint lengthInBlocks,
+                                                          IReadOnlyList<(ulong Start, ulong End)> sectorExtents)
+    {
+        List<(ulong Start, ulong End)> overlaps = [];
+
+        ulong startSector = _partition.Start             + startBlock;
+        ulong endSector   = startSector + lengthInBlocks - 1;
+
+        AddExtentOverlaps(startSector, endSector, sectorExtents, overlaps);
+
+        return NormalizeAnalyzeExtents(overlaps);
+    }
+
+    static void AddExtentOverlaps(ulong                                   startSector, ulong endSector,
+                                  IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                                  List<(ulong Start, ulong End)>          overlaps)
+    {
+        foreach((ulong Start, ulong End) requestedExtent in sectorExtents)
+        {
+            if(requestedExtent.End < startSector || requestedExtent.Start > endSector) continue;
+
+            overlaps.Add((Math.Max(startSector, requestedExtent.Start), Math.Min(endSector, requestedExtent.End)));
+        }
+    }
+
+    static List<(ulong Start, ulong End)> NormalizeAnalyzeExtents(IEnumerable<(ulong Start, ulong End)> extents)
+    {
+        var orderedExtents = extents.Where(static extent => extent.End >= extent.Start)
+                                    .OrderBy(static extent => extent.Start)
+                                    .ThenBy(static extent => extent.End)
+                                    .ToList();
+
+        List<(ulong Start, ulong End)> normalizedExtents = [];
+
+        if(orderedExtents.Count == 0) return normalizedExtents;
+
+        (ulong Start, ulong End) currentExtent = orderedExtents[0];
+
+        for(var i = 1; i < orderedExtents.Count; i++)
+        {
+            if(orderedExtents[i].Start <= currentExtent.End + 1)
+            {
+                currentExtent.End = Math.Max(currentExtent.End, orderedExtents[i].End);
+
+                continue;
+            }
+
+            normalizedExtents.Add(currentExtent);
+            currentExtent = orderedExtents[i];
+        }
+
+        normalizedExtents.Add(currentExtent);
+
+        return normalizedExtents;
+    }
+}
