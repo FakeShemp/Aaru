3da371c2a5c42d843ae3e7bae632f5d8d1114c56
Natalia Portillo <claunia@claunia.com>
2026-04-10 23:23:55 +0100

    [UCSDPascal] Add support for analyze command

---
+++ Aaru.Filesystems/UCSDPascal/Analyze.cs
@@ -0,0 +1,178 @@
+// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : Analyze.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : U.C.S.D. Pascal filesystem plugin.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2026 Natalia Portillo
+// ****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aaru.CommonTypes;
+using Aaru.CommonTypes.Enums;
+using Aaru.CommonTypes.Structs;
+using Aaru.Helpers;
+
+namespace Aaru.Filesystems;
+
+// Information from Call-A.P.P.L.E. Pascal Disk Directory Structure
+/// <inheritdoc />
+public sealed partial class PascalPlugin
+{
+#region IReadOnlyFilesystem Members
+
+    /// <inheritdoc />
+    public ErrorNumber GetFilesWithAffectedSectors(IEnumerable<(ulong Start, ulong End)> sectorExtents,
+                                                   out List<FileSectorInfo>              files,
+                                                   InitProgressHandler                   initProgress   = null,
+                                                   UpdateProgressHandler                 updateProgress = null,
+                                                   PulseProgressHandler                  pulseProgress  = null,
+                                                   EndProgressHandler                    endProgress    = null)
+    {
+        files = [];
+
+        if(!_mounted) return ErrorNumber.AccessDenied;
+        if(sectorExtents is null) return ErrorNumber.InvalidArgument;
+
+        List<(ulong Start, ulong End)> normalizedExtents = NormalizeAnalyzeExtents(sectorExtents);
+
+        if(normalizedExtents.Count == 0 || _fileEntries is null || _fileEntries.Count == 0) return ErrorNumber.NoError;
+
+        try
+        {
+            initProgress?.Invoke();
+            pulseProgress?.Invoke("/");
+
+            PascalFileEntry[] orderedEntries = _fileEntries.Where(static entry => entry.Filename?[0] > 0)
+                                                           .OrderBy(entry =>
+                                                                        StringHandlers.PascalToString(entry.Filename,
+                                                                            _encoding),
+                                                                    StringComparer.CurrentCultureIgnoreCase)
+                                                           .ToArray();
+
+            long maximum = orderedEntries.Length > 0 ? orderedEntries.Length : 1;
+
+            for(var i = 0; i < orderedEntries.Length; i++)
+            {
+                string entryName = StringHandlers.PascalToString(orderedEntries[i].Filename, _encoding);
+
+                if(string.IsNullOrWhiteSpace(entryName)) continue;
+
+                string entryPath = "/" + entryName;
+
+                updateProgress?.Invoke(entryPath, i + 1, maximum);
+
+                AddOverlappingFile(entryPath, orderedEntries[i], normalizedExtents, files);
+            }
+
+            return ErrorNumber.NoError;
+        }
+        finally
+        {
+            endProgress?.Invoke();
+        }
+    }
+
+#endregion
+
+    void AddOverlappingFile(string path, PascalFileEntry entry, IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                            List<FileSectorInfo> files)
+    {
+        if(entry.FirstBlock < 0 || entry.LastBlock <= entry.FirstBlock) return;
+
+        List<(ulong Start, ulong End)> overlaps = FindOverlappingExtents(entry, sectorExtents);
+
+        if(overlaps.Count == 0) return;
+
+        files.Add(new FileSectorInfo
+        {
+            Path            = path,
+            Inode           = (ulong)entry.FirstBlock,
+            AffectedSectors = overlaps
+        });
+    }
+
+    List<(ulong Start, ulong End)> FindOverlappingExtents(PascalFileEntry                         entry,
+                                                          IReadOnlyList<(ulong Start, ulong End)> sectorExtents)
+    {
+        List<(ulong Start, ulong End)> overlaps = [];
+        long                           fileSize = GetFileSize(entry);
+
+        if(fileSize <= 0) return overlaps;
+
+        ulong startSector = (ulong)entry.FirstBlock                         * _multiplier;
+        ulong sectorCount = ((ulong)fileSize + _device.Info.SectorSize - 1) / _device.Info.SectorSize;
+
+        if(sectorCount == 0) sectorCount = (ulong)(entry.LastBlock - entry.FirstBlock) * _multiplier;
+
+        if(sectorCount == 0) return overlaps;
+
+        AddExtentOverlaps(startSector, startSector + sectorCount - 1, sectorExtents, overlaps);
+
+        return NormalizeAnalyzeExtents(overlaps);
+    }
+
+    static void AddExtentOverlaps(ulong                                   startSector, ulong endSector,
+                                  IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                                  List<(ulong Start, ulong End)>          overlaps)
+    {
+        foreach((ulong Start, ulong End) requestedExtent in sectorExtents)
+        {
+            if(requestedExtent.End < startSector || requestedExtent.Start > endSector) continue;
+
+            overlaps.Add((Math.Max(startSector, requestedExtent.Start), Math.Min(endSector, requestedExtent.End)));
+        }
+    }
+
+    static List<(ulong Start, ulong End)> NormalizeAnalyzeExtents(IEnumerable<(ulong Start, ulong End)> extents)
+    {
+        var orderedExtents = extents.Where(static extent => extent.End >= extent.Start)
+                                    .OrderBy(static extent => extent.Start)
+                                    .ThenBy(static extent => extent.End)
+                                    .ToList();
+
+        List<(ulong Start, ulong End)> normalizedExtents = [];
+
+        if(orderedExtents.Count == 0) return normalizedExtents;
+
+        (ulong Start, ulong End) currentExtent = orderedExtents[0];
+
+        for(var i = 1; i < orderedExtents.Count; i++)
+        {
+            if(orderedExtents[i].Start <= currentExtent.End + 1)
+            {
+                currentExtent.End = Math.Max(currentExtent.End, orderedExtents[i].End);
+
+                continue;
+            }
+
+            normalizedExtents.Add(currentExtent);
+            currentExtent = orderedExtents[i];
+        }
+
+        normalizedExtents.Add(currentExtent);
+
+        return normalizedExtents;
+    }
+}
