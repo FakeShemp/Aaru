65387466f986c57773a618858453b332a454cc4f
Natalia Portillo <claunia@claunia.com>
2026-04-11 08:47:54 +0100

    [BeFS] Add support for analyze command

---
+++ Aaru.Filesystems/BFS/Analyze.cs
@@ -0,0 +1,261 @@
+// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : Analyze.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : BeOS filesystem plugin.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2026 Natalia Portillo
+// ****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aaru.CommonTypes;
+using Aaru.CommonTypes.Enums;
+using Aaru.CommonTypes.Interfaces;
+using Aaru.CommonTypes.Structs;
+
+namespace Aaru.Filesystems;
+
+// Information from Practical Filesystem Design, ISBN 1-55860-497-9
+/// <inheritdoc />
+/// <summary>Implements detection of the Be (new) filesystem</summary>
+public sealed partial class BeFS
+{
+#region IReadOnlyFilesystem Members
+
+    /// <inheritdoc />
+    public ErrorNumber GetFilesWithAffectedSectors(IEnumerable<(ulong Start, ulong End)> sectorExtents,
+                                                   out List<FileSectorInfo>              files,
+                                                   InitProgressHandler                   initProgress   = null,
+                                                   UpdateProgressHandler                 updateProgress = null,
+                                                   PulseProgressHandler                  pulseProgress  = null,
+                                                   EndProgressHandler                    endProgress    = null)
+    {
+        files = [];
+
+        if(!_mounted) return ErrorNumber.AccessDenied;
+        if(sectorExtents is null) return ErrorNumber.InvalidArgument;
+
+        List<(ulong Start, ulong End)> normalizedExtents = NormalizeAnalyzeExtents(sectorExtents);
+
+        if(normalizedExtents.Count == 0) return ErrorNumber.NoError;
+
+        try
+        {
+            initProgress?.Invoke();
+
+            return TraverseDirectoryForAffectedSectors("/",
+                                                       normalizedExtents,
+                                                       files,
+                                                       updateProgress,
+                                                       pulseProgress);
+        }
+        finally
+        {
+            endProgress?.Invoke();
+        }
+    }
+
+#endregion
+
+    ErrorNumber TraverseDirectoryForAffectedSectors(string path,
+                                                    IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                                                    List<FileSectorInfo> files,
+                                                    UpdateProgressHandler updateProgress,
+                                                    PulseProgressHandler pulseProgress)
+    {
+        pulseProgress?.Invoke(path);
+
+        ErrorNumber errno = OpenDir(path, out IDirNode node);
+
+        if(errno != ErrorNumber.NoError) return errno;
+
+        try
+        {
+            List<string> entries = [];
+
+            while(true)
+            {
+                errno = ReadDir(node, out string entryName);
+
+                if(errno != ErrorNumber.NoError) return errno;
+                if(entryName is null) break;
+
+                entries.Add(entryName);
+            }
+
+            string[] orderedEntries = entries.Where(static entry =>
+                                                                !string.IsNullOrWhiteSpace(entry) &&
+                                                                entry is not "." and not "..")
+                                             .OrderBy(static entry => entry, StringComparer.Ordinal)
+                                             .ToArray();
+            long maximum = orderedEntries.Length > 0 ? orderedEntries.Length : 1;
+
+            for(int i = 0; i < orderedEntries.Length; i++)
+            {
+                string entryPath = path == "/" ? "/" + orderedEntries[i] : path + "/" + orderedEntries[i];
+
+                updateProgress?.Invoke(entryPath, i + 1, maximum);
+
+                errno = Stat(entryPath, out FileEntryInfo stat);
+
+                if(errno != ErrorNumber.NoError) return errno;
+
+                if(stat.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    errno = TraverseDirectoryForAffectedSectors(entryPath,
+                                                                sectorExtents,
+                                                                files,
+                                                                updateProgress,
+                                                                pulseProgress);
+
+                    if(errno != ErrorNumber.NoError) return errno;
+
+                    continue;
+                }
+
+                errno = AddOverlappingFile(entryPath, sectorExtents, files);
+
+                if(errno != ErrorNumber.NoError) return errno;
+            }
+
+            return ErrorNumber.NoError;
+        }
+        finally
+        {
+            CloseDir(node);
+        }
+    }
+
+    ErrorNumber AddOverlappingFile(string path, IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                                   List<FileSectorInfo> files)
+    {
+        ErrorNumber errno = GetInodeForPath(path, out bfs_inode inode);
+
+        if(errno != ErrorNumber.NoError) return errno;
+        if(inode.data.size <= 0) return ErrorNumber.NoError;
+
+        errno = FindOverlappingExtents(inode.data, sectorExtents, out List<(ulong Start, ulong End)> overlaps);
+
+        if(errno != ErrorNumber.NoError) return errno;
+        if(overlaps.Count == 0) return ErrorNumber.NoError;
+
+        ulong inodeNumber = (ulong)inode.inode_num.allocation_group << _superblock.ag_shift | inode.inode_num.start;
+
+        files.Add(new FileSectorInfo
+        {
+            Path            = path,
+            Inode           = inodeNumber,
+            AffectedSectors = overlaps
+        });
+
+        return ErrorNumber.NoError;
+    }
+
+    ErrorNumber FindOverlappingExtents(data_stream dataStream,
+                                       IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                                       out List<(ulong Start, ulong End)> overlaps)
+    {
+        overlaps = [];
+
+        ulong remainingBytes = (ulong)Math.Max(dataStream.size, 0);
+
+        for(int i = 0; i < NUM_DIRECT_BLOCKS && remainingBytes > 0; i++)
+        {
+            if(dataStream.direct[i].len == 0) break;
+
+            long  blockStart = ((long)dataStream.direct[i].allocation_group << _superblock.ag_shift) +
+                               dataStream.direct[i].start;
+            ulong blockBytes = (ulong)dataStream.direct[i].len * (ulong)_superblock.block_size;
+            ulong extentBytes = Math.Min(blockBytes, remainingBytes);
+
+            AddByteRangeOverlaps((ulong)blockStart * (ulong)_superblock.block_size + (ulong)_volumeOffset,
+                                 extentBytes,
+                                 sectorExtents,
+                                 overlaps);
+            remainingBytes -= extentBytes;
+        }
+
+        overlaps = NormalizeAnalyzeExtents(overlaps);
+
+        return ErrorNumber.NoError;
+    }
+
+    void AddByteRangeOverlaps(ulong absoluteByteOffset, ulong lengthBytes,
+                              IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                              List<(ulong Start, ulong End)> overlaps)
+    {
+        if(lengthBytes == 0) return;
+
+        ulong sectorSize     = _imagePlugin.Info.SectorSize;
+        ulong startSector    = _partition.Start + absoluteByteOffset / sectorSize;
+        ulong offsetInSector = absoluteByteOffset % sectorSize;
+        ulong sectorCount    = (lengthBytes + offsetInSector + sectorSize - 1) / sectorSize;
+
+        if(sectorCount == 0) sectorCount = 1;
+
+        AddExtentOverlaps(startSector, startSector + sectorCount - 1, sectorExtents, overlaps);
+    }
+
+    static void AddExtentOverlaps(ulong startSector, ulong endSector,
+                                  IReadOnlyList<(ulong Start, ulong End)> sectorExtents,
+                                  List<(ulong Start, ulong End)> overlaps)
+    {
+        foreach((ulong Start, ulong End) requestedExtent in sectorExtents)
+        {
+            if(requestedExtent.End < startSector || requestedExtent.Start > endSector) continue;
+
+            overlaps.Add((Math.Max(startSector, requestedExtent.Start), Math.Min(endSector, requestedExtent.End)));
+        }
+    }
+
+    static List<(ulong Start, ulong End)> NormalizeAnalyzeExtents(IEnumerable<(ulong Start, ulong End)> extents)
+    {
+        List<(ulong Start, ulong End)> orderedExtents = extents.Where(static extent => extent.End >= extent.Start)
+                                                               .OrderBy(static extent => extent.Start)
+                                                               .ThenBy(static extent => extent.End)
+                                                               .ToList();
+        List<(ulong Start, ulong End)> normalizedExtents = [];
+
+        if(orderedExtents.Count == 0) return normalizedExtents;
+
+        (ulong Start, ulong End) currentExtent = orderedExtents[0];
+
+        for(int i = 1; i < orderedExtents.Count; i++)
+        {
+            if(orderedExtents[i].Start <= currentExtent.End + 1)
+            {
+                currentExtent.End = Math.Max(currentExtent.End, orderedExtents[i].End);
+
+                continue;
+            }
+
+            normalizedExtents.Add(currentExtent);
+            currentExtent = orderedExtents[i];
+        }
+
+        normalizedExtents.Add(currentExtent);
+
+        return normalizedExtents;
+    }
+}
