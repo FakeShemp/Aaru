438326bc4270ee604f6e31e9c03efaa1a98fe26b
Natalia Portillo <claunia@claunia.com>
2026-04-20 23:16:04 +0100

    Add KenCode class implementing Apple KenCode compression algorithm. Fixes #122

---
+++ Aaru.Compression/Apple/KenCode.cs
@@ -0,0 +1,58 @@
+// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : KenCode.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : Compression algorithms.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2026 Natalia Portillo
+// ****************************************************************************/
+
+using System.Runtime.InteropServices;
+
+namespace Aaru.Compression.Apple;
+
+// ReSharper disable once InconsistentNaming
+/// <summary>Implements the Apple KenCode compression algorithm</summary>
+public partial class KenCode
+{
+    /// <summary>Set to <c>true</c> if this algorithm is supported, <c>false</c> otherwise.</summary>
+    public static bool IsSupported => Native.IsSupported;
+
+    [LibraryImport("libAaru.Compression.Native", SetLastError = true)]
+    private static partial int AARU_kencode_decode_buffer(byte[] dst_buffer, ref nint dst_size, byte[] src_buffer,
+                                                          nint   src_size);
+
+    /// <summary>Decodes a buffer compressed with Apple's LZH</summary>
+    /// <param name="source">Encoded buffer</param>
+    /// <param name="destination">Buffer where to write the decoded data</param>
+    /// <returns>The number of decoded bytes</returns>
+    public static int DecodeBuffer(byte[] source, byte[] destination)
+    {
+        if(!Native.IsSupported) return 0;
+
+        nint destLen = destination.Length;
+
+        AARU_kencode_decode_buffer(destination, ref destLen, source, source.Length);
+
+        return (int)destLen;
+    }
+}
